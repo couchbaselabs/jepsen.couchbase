6bd1c69e191bf557261e059024fdd2c296df8adf
dave-finlay <dave.finlay@couchbase.com>
2019-06-05 10:38:57 -0700

    Remove the dcp-client as a dependency and add as explicit jar
    The java-dcp-client currently isn't available on the couchbase
    maven preview repo https://files.couchbase.com/maven2. For now,
    let's do the practical thing of adding this dependency as an
    explicit jar file while we wait for this dependency to be
    available on a maven repository.
    
    Change-Id: I5d0378172c48847ebb0adead8596d96bbbbbdee0
    Reviewed-on: http://review.couchbase.org/110244
    Tested-by: Dave Finlay <dave.finlay@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [jepsen "0.1.11"]
                  [com.couchbase.client/java-client "3.0.0-alpha.2"]
-                 [com.couchbase.client/dcp-client "0.21.0-SHADEDCORE"]
                  [io.projectreactor/reactor-core "3.2.1.RELEASE"]
                  [io.projectreactor.addons/reactor-adapter "3.2.0.RELEASE"]
                  [io.projectreactor.addons/reactor-extra "3.2.0.RELEASE"]
                  [io.reactivex/rxjava-reactive-streams "1.2.1"]
                  [cheshire "5.8.1"]
                  [clj-http "3.9.1"]]
-  :resource-paths ["./lib/couchbase-transactions-1.0.0-alpha.3.jar"]
+  :resource-paths ["./lib/couchbase-transactions-1.0.0-alpha.3.jar"
+                   "./lib/dcp-client-0.21.0-SHADEDCORE.jar"]
   :repositories [["couchbase-preview" {:url "https://files.couchbase.com/maven2"
                                        :checksum :ignore}]]
   :jvm-opts ~(if (-> (System/getProperty "java.version")
