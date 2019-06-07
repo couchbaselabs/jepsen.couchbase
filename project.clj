df7a2907ecc39f893ec3b72b5c0d88e025cf7190
dave-finlay <dave.finlay@couchbase.com>
2019-06-06 17:02:06 -0700

    Correctly reflect that the dcp-client jar is based off 0.23.0
    When I first built the dcp-client jar and merged it into this repo,
    I listed it as built off 0.21.0. In fact it was really built off
    0.23.0 code. I've build it again off exactly 0.23.0 and am readding
    it to this repository under the correct name.
    
    The pom file to regenerate the dcp-client jar can be found here:
    http://review.couchbase.org/#/c/110267/.
    
    Additionally, I'm reworking the dependencies into a more natural
    order with more basic dependencies first and more complex
    dependencies later.
    
    Change-Id: Ib32a91a6a5e7982ea5c84c98300749902c609113
    Reviewed-on: http://review.couchbase.org/110328
    Tested-by: Dave Finlay <dave.finlay@couchbase.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :plugins [[lein-cljfmt "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [jepsen "0.1.11"]
-                 [com.couchbase.client/core-io "2.0.0-alpha.3"]
-                 [com.couchbase.client/java-client "3.0.0-alpha.3"]
                  [io.projectreactor/reactor-core "3.2.1.RELEASE"]
                  [io.projectreactor.addons/reactor-adapter "3.2.0.RELEASE"]
                  [io.projectreactor.addons/reactor-extra "3.2.0.RELEASE"]
                  [io.reactivex/rxjava-reactive-streams "1.2.1"]
                  [cheshire "5.8.1"]
-                 [clj-http "3.9.1"]]
-  :resource-paths ["./lib/couchbase-transactions-1.0.0-alpha.3.jar"
-                   "./lib/dcp-client-0.21.0-SHADEDCORE.jar"]
+                 [clj-http "3.9.1"]
+                 [com.couchbase.client/core-io "2.0.0-alpha.3"]
+                 [com.couchbase.client/java-client "3.0.0-alpha.3"]]
+  :resource-paths ["./lib/dcp-client-0.23.0-SHADEDCORE.jar"
+                   "./lib/couchbase-transactions-1.0.0-alpha.3.jar"]
   :repositories [["couchbase-preview" {:url "https://files.couchbase.com/maven2"
                                        :checksum :ignore}]]
   :jvm-opts ~(if (-> (System/getProperty "java.version")
