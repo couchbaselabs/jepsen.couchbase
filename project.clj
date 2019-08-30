7f53ec164687b5aefbb09bd86be4f589e30d587c
Richard de Mellow <richard.demellow@couchbase.com>
2019-08-30 10:17:50 +0100

    MB-35753 Update project.cli to the latest version of each libs we use
    Change-Id: I404ccc26dddf01865ff47d7796c76f21c16516f1
    Reviewed-on: http://review.couchbase.org/114056
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Sven Signer <sven.signer@couchbase.com>

--- project.clj
+++ project.clj
@@ -2,26 +2,15 @@
   :description "Jepsen testing for couchbase"
   :main couchbase.core
   :plugins [[lein-cljfmt "0.6.4"]
-            [lein-kibit "0.1.6"]]
+            [lein-kibit "0.1.7"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [jepsen "0.1.14"]
-                 [io.projectreactor/reactor-core "3.2.1.RELEASE"]
-                 [io.projectreactor.addons/reactor-adapter "3.2.0.RELEASE"]
-                 [io.projectreactor.addons/reactor-extra "3.2.0.RELEASE"]
-                 [io.reactivex/rxjava-reactive-streams "1.2.1"]
-                 [cheshire "5.8.1"]
-                 [clj-http "3.9.1"]
+                 [jepsen "0.1.15"]
+                 [cheshire "5.9.0"]
+                 [clj-http "3.10.0"]
                  [com.couchbase.client/core-io "2.0.0-alpha.6"]
                  [com.couchbase.client/java-client "3.0.0-alpha.6"]]
   :resource-paths ["./lib/dcp-client-0.23.0-SHADEDCORE.jar"
                    "./lib/couchbase-transactions-1.0.0-alpha.3.jar"]
-  :repositories [["couchbase-preview" {:url "https://files.couchbase.com/maven2"
-                                       :checksum :ignore}]]
-  :jvm-opts ~(if (-> (System/getProperty "java.version")
-                     (clojure.string/split #"\.")
-                     (first)
-                     (Integer/parseInt)
-                     (>= 9))
-               ["--add-modules" "java.xml.bind"]
-               []))
+  :repositories [["couchbase-preview" {:url      "https://files.couchbase.com/maven2"
+                                       :checksum :ignore}]])
