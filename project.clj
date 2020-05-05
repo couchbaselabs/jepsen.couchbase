0f34efa54c8100f2e58da6888797f638bb5fb966
Richard de Mellow <richard.demellow@couchbase.com>
2020-05-05 14:03:46 +0100

    Update jepsen.couchbase dependencies
    Update project.clj to use the latest version of the dependencies that
    are currently available. However, we will not update
    couchbase-transactions SDK and jepsen as they both require large
    changes to our framework.
    
    Change-Id: I4dfefa690bf4f70cea5e27a9bff6b3e22cc1d3f8
    Reviewed-on: http://review.couchbase.org/c/jepsen.couchbase/+/127298
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
 (defproject couchbase "0.1.0-SNAPSHOT"
   :description "Jepsen testing for couchbase"
   :main couchbase.core
-  :plugins [[lein-cljfmt "0.6.4"]
-            [lein-kibit "0.1.7"]
-            [jonase/eastwood "0.3.5"]]
+  :plugins [[lein-cljfmt "0.6.7"]
+            [lein-kibit "0.1.8"]
+            [jonase/eastwood "0.3.11"]]
   :jvm-opts ["-server"       ; Be more aggressive with optimisations
              "-Xms32G"       ; Set the starting heap size to 32GB and the max heap size to 32GB so that we never
              "-Xmx32G"       ; have to re-size the heap which causes the GC to be used. If we go above 32GB we will
                              ; crash but we shouldn't be using that much memory in a normal situation anyway
              "-XX:+UseG1GC"] ; Use Garbage First (G1) Collector, this should reduce pause time over reducing memory
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "0.4.2"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [jepsen "0.1.15"]
-                 [cheshire "5.9.0"]
-                 [clj-http "3.10.0"]
-                 [com.couchbase.client/core-io "2.0.0"]
-                 [com.couchbase.client/java-client "3.0.0"]
+                 [cheshire "5.10.0"]
+                 [clj-http "3.10.1"]
+                 [com.couchbase.client/core-io "2.0.4"]
+                 [com.couchbase.client/java-client "3.0.3"]
                  [com.couchbase.client/couchbase-transactions "1.0.0-beta.3-SNAPSHOT"]]
   :resource-paths ["./lib/dcp-client-0.23.0-SHADEDCORE.jar"]
   :repositories [["couchbase-snapshot" {:url      "https://oss.sonatype.org/content/repositories/snapshots"
