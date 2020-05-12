a79b77a3dfb95818510aa9deeaa08ac872ab7552
Richard de Mellow <richard.demellow@couchbase.com>
2020-05-12 15:10:13 +0100

    Ensure we remove old Couchbase server builds
    Ensure the removal of old serer builds in the root directory before
    moving newly downloaded build too the repo root directory. For the test
    to pickup.
    
    Change-Id: I571f396494702312cf15b429d7f52c0447b841b1
    Reviewed-on: http://review.couchbase.org/c/jepsen.couchbase/+/127875
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- cv-job-scripts/kv-cv-job.sh
+++ cv-job-scripts/kv-cv-job.sh
@@ -120,6 +120,8 @@ function download_build() {
     wget -4 -o download.log "http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-server/$BRANCH/latest/$PACKAGE_NAME"
     BUILD_VERSION=$(dpkg-deb -f ${PACKAGE_NAME} Version)
     echo "Couchbase Server: $BUILD_VERSION"
+    rm -fv "${GIT_ROOT_TEST}/*.deb"
+    rm -fv "${GIT_ROOT_TEST}/*.rpm"
     mv -f ./${PACKAGE_NAME} ${GIT_ROOT_TEST}
 }
 
