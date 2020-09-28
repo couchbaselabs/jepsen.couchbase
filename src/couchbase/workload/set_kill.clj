ee5818a03d2401c65802af41f26f71c85b22d970
Sven Signer <sven.signer@couchbase.com>
2020-09-28 08:43:31 +0100

    MB-37436 (17/n) Port set-kill workload
    Change-Id: I87dca51cfb3f3686cb11ea1f2801ff5e9be3874d
    Reviewed-on: http://review.couchbase.org/c/jepsen.couchbase/+/137018
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

---
+++ src/couchbase/workload/set_kill.clj
@@ -0,0 +1,155 @@
+(ns couchbase.workload.set-kill
+  (:require [couchbase
+             [clients :as clients]
+             [nemesis :as nemesis]
+             [util :as util]
+             [workload :as workload]]
+            [jepsen.generator.pure :as gen]))
+
+;; Action Helpers
+
+(defn kill-memcached-slow-disk-cycle
+  [opts]
+  [(gen/sleep 10)
+   {:type :info
+    :f :slow-disk
+    :targeter nemesis/target-all-test-nodes}
+   (gen/sleep 4)
+   {:type :info
+    :f :kill-process
+    :kill-process :memcached
+    :targeter nemesis/basic-nodes-targeter
+    :target-count (opts :disrupt-count)}
+   {:type :info
+    :f :reset-disk
+    :targeter nemesis/target-all-test-nodes}
+   (gen/sleep 10)])
+
+(defn kill-memcached-cycle
+  [opts]
+  [(gen/sleep 10)
+   {:type :info
+    :f :kill-process
+    :kill-process :memcached
+    :targeter nemesis/basic-nodes-targeter
+    :target-count (opts :disrupt-count)}
+   (gen/sleep 20)
+   ;; We might need to rebalance the cluster if we're testing
+   ;; against ephemeral so we can read data back see MB-36800
+   {:type :info
+    :f :rebalance-cluster}])
+
+(defn kill-ns-server-cycle
+  [opts]
+  [(gen/sleep 10)
+   {:type :info
+    :f :kill-process
+    :kill-process :ns-server
+    :targeter nemesis/basic-nodes-targeter
+    :target-count (opts :disrupt-count)}
+   (gen/sleep 20)])
+
+(defn kill-babysitter-cycle
+  [opts]
+  (let [targeter (nemesis/start-stop-targeter)]
+    [(gen/sleep 5)
+     {:type :info
+      :f :kill-process
+      :kill-process :babysitter
+      :targeter targeter
+      :target-count (opts :disrupt-count)
+      :target-action :start}
+     (gen/sleep (opts :disrupt-time))
+     {:type :info
+      :f :start-process
+      :targeter targeter
+      :target-count (opts :disrupt-count)
+      :target-action :stop}
+     (gen/sleep 5)
+     {:type :info
+      :f :recover
+      :recovery-type (opts :recovery-type)}
+     (gen/sleep 10)]))
+
+(defn hard-reboot-cycle
+  [opts]
+  [(gen/sleep 10)
+   {:type :info
+    :f :hard-reboot
+    :targeter nemesis/basic-nodes-targeter
+    :target-count (opts :disrupt-count)}
+   (gen/sleep 60)
+   ;; We might need to rebalance the cluster if we're testing
+   ;; against ephemeral so we can read data back see MB-36800
+   {:type :info
+    :f :rebalance-cluster}])
+
+(defn suspend-process-cycle
+  [opts]
+  (let [targeter (nemesis/start-stop-targeter)]
+    [(gen/sleep 10)
+     {:type :info
+      :f :halt-process
+      :target-process (opts :process-to-suspend)
+      :targeter targeter
+      :target-count (opts :disrupt-count)
+      :target-action :start}
+     (gen/sleep (:process-suspend-time opts))
+     {:type :info
+      :f :continue-process
+      :target-process (:process-to-suspend opts)
+      :targeter-process (:process-to-suspend opts)
+      :targeter targeter
+      :target-count (opts :disrupt-count)
+      :target-action :stop}
+     (gen/sleep 5)]))
+
+(defn nemesis-cycle
+  [opts]
+  (case (:scenario opts)
+    :kill-memcached-on-slow-disk (kill-memcached-slow-disk-cycle opts)
+    :kill-memcached (kill-memcached-cycle opts)
+    :kill-ns-server (kill-ns-server-cycle opts)
+    :kill-babysitter (kill-babysitter-cycle opts)
+    :hard-reboot (hard-reboot-cycle opts)
+    :suspend-process (suspend-process-cycle opts)
+    (throw (RuntimeException. "Set-kill workload requires scenario"))))
+
+;; Generators
+
+(defn nemesis-gen
+  [opts]
+  [(repeatedly (opts :cycles) #(nemesis-cycle opts))
+   (gen/sleep 10)
+   {:type :stop-test}])
+
+(defn client-gen
+  [opts]
+  (map (fn [x] {:f :add
+                :value x
+                :replicate-to (:replicate-to opts)
+                :persist-to (:persist-to opts)
+                :durability-level (util/random-durability-level
+                                   (:durability opts))
+                :json (:use-json-docs opts)})
+       (range)))
+
+(defn combined-gen
+  [opts]
+  (workload/wrap-set-generator
+   (gen/phases
+    (workload/wrap-generators (client-gen opts) (nemesis-gen opts))
+    {:f :read :value nil})))
+
+(defn workload-opts [opts]
+  {:workload-type :set
+   :custom-vbucket-count (:custom-vbucket-count opts 64)
+   :disrupt-time (:disrupt-time opts 30)})
+
+(defn workload-fn [opts]
+  (merge
+   (workload/set-common opts :basic)
+   {:pure-generators true
+    :concurrency 1000
+    :nemesis (nemesis/couchbase)
+    :generator (combined-gen opts)}))
