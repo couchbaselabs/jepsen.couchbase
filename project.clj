b29a33ceb947358fa6b21396a00fd2c7ca271da2
Richard de Mellow <richard.demellow@couchbase.com>
2020-12-17 16:13:28 +0000

    Bump sdk and library versions
    Change-Id: I7d0a0ce2a3ffffe8aab5efb91b782568a13bdf06
    Reviewed-on: http://review.couchbase.org/c/jepsen.couchbase/+/142372
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject couchbase "0.1.0-SNAPSHOT"
   :description "Jepsen testing for couchbase"
   :main couchbase.core
-  :plugins [[lein-cljfmt "0.6.7"]
+  :plugins [[lein-cljfmt "0.7.0"]
             [lein-kibit "0.1.8"]
-            [jonase/eastwood "0.3.11"]]
+            [jonase/eastwood "0.3.12"]]
   :jvm-opts ["-server"       ; Be more aggressive with optimisations
              "-Xms32G"       ; Set the starting heap size to 32GB and the max heap size to 32GB so that we never
              "-Xmx32G"       ; have to re-size the heap which causes the GC to be used. If we go above 32GB we will
@@ -13,10 +13,10 @@
                  [org.clojure/tools.cli "1.0.194"]
                  [jepsen "0.2.1"]
                  [cheshire "5.10.0"]
-                 [clj-http "3.10.1"]
-                 [com.couchbase.client/core-io "2.0.8"]
-                 [com.couchbase.client/java-client "3.0.7"]
-                 [com.couchbase.client/dcp-client "0.29.0"]
+                 [clj-http "3.11.0"]
+                 [com.couchbase.client/core-io "2.1.0"]
+                 [com.couchbase.client/java-client "3.1.0"]
+                 [com.couchbase.client/dcp-client "0.31.0"]
                  [com.couchbase.client/couchbase-transactions "1.0.0-beta.3-SNAPSHOT"]]
   :profiles {:dev {:resource-paths ["./test/couchbase/resources"]}
              :uberjar {:aot :all}}
