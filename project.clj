45e78e4f89552861c7ce05ac669c9171956ef81d
Richard de Mellow <richard.demellow@couchbase.com>
2023-01-24 14:34:53 +0000

    Revert "Update dependencies & plugin versions in project.cli"
    This reverts commit 04494995fee27d6fb264c1e19b0d9f2ff37e13e1.
    
    Reason for revert: More work need to be done to address crash when
    the Jepsen tests run.
    
    Change-Id: If342e262852b5b0f083bc63a877e5cc55fc5dc0e
    Reviewed-on: https://review.couchbase.org/c/jepsen.couchbase/+/185107
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject couchbase "0.1.0-SNAPSHOT"
   :description "Jepsen testing for couchbase"
   :main couchbase.core
-  :plugins [[lein-cljfmt "0.9.0"]
+  :plugins [[lein-cljfmt "0.7.0"]
             [lein-kibit "0.1.8"]
             [jonase/eastwood "0.3.12"]]
   :jvm-opts ["-server"       ; Be more aggressive with optimisations
@@ -9,15 +9,14 @@
              "-Xmx32G"       ; have to re-size the heap which causes the GC to be used. If we go above 32GB we will
                              ; crash but we shouldn't be using that much memory in a normal situation anyway
              "-XX:+UseG1GC"] ; Use Garbage First (G1) Collector, this should reduce pause time over reducing memory
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.cli "1.0.214"]
-                 [org.slf4j/slf4j-simple "2.0.6"]
-                 [jepsen "0.3.1"]
-                 [cheshire "5.11.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.cli "1.0.206"]
+                 [jepsen "0.2.5"]
+                 [cheshire "5.10.1"]
                  [clj-http "3.12.3"]
-                 [com.couchbase.client/core-io "2.4.0"]
-                 [com.couchbase.client/java-client "3.4.0"]
-                 [com.couchbase.client/dcp-client "0.42.0"]]
+                 [com.couchbase.client/core-io "2.2.3"]
+                 [com.couchbase.client/java-client "3.2.3"]
+                 [com.couchbase.client/dcp-client "0.37.0"]]
   :profiles {:dev {:resource-paths ["./test/couchbase/resources"]}
              :uberjar {:aot :all}}
   :repositories [["couchbase-snapshot" {:url      "https://oss.sonatype.org/content/repositories/snapshots"
